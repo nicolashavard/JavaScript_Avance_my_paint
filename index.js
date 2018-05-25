07a50b453c6ad05abe9b65c76b919961eb09082a
Edgear <nicolas.havard@epitech.eu>
2018-05-25 16:32:02 +0200

    Symetrie parfaite

--- index.js
+++ index.js
@@ -85,86 +85,79 @@ function readURL(input) {
 
     if (input.files && input.files[0]) {
 
-        var reader = new FileReader();
         var img = new Image();
-        reader.readAsDataURL(input.files[0]);
-
-        console.log(reader);
-
-        reader.onload = function (e) {
-
-            img.src = e.target.result;
-            console.log(img.src);
-
-            var img_width = img.width > ctx.width ? canvas.width : img.width;
-            var img_height = img.height > ctx.height ? canvas.height : img.height;
-
-            ctx.drawImage(img,0,0,img_width,img_height);
-
-        }
+        img.onload = function () {
+            ctx.drawImage(img, 0, 0);
+        };
+        img.src = URL.createObjectURL(input.files[0]);
+        var img_width = img.width > ctx.width ? canvas.width : img.width;
+        var img_height = img.height > ctx.height ? canvas.height : img.height;
+        ctx.drawImage(img, 0, 0, img_width, img_height);
     }
 }
 
-
-
-// todo getImageData()
-function Draw(x, y, isDown) {
+function trace(mx, my, lx, ly, circle = false) {
+    console.log(circle);
     ctx.beginPath();
     ctx.strokeStyle = $('#colorpick').val();
     ctx.lineWidth = $('#sizepick').val();
     ctx.lineJoin = "round";
+    ctx.moveTo(mx, my);
+    if(circle){
+        ctx.arc(lx, ly, 0.1, 0, Math.PI * 2, false);
+    } else {
+        ctx.lineTo(lx, ly);
+    }
+    ctx.closePath();
+    ctx.stroke();
+}
+
+// todo getImageData()
+function Draw(x, y, isDown) {
     if (isDown) {
         if(tool === 'eraser'){
             ctx.globalCompositeOperation="destination-out"
         } else {
             ctx.globalCompositeOperation="source-over";
         }
-        ctx.moveTo(lastX, lastY);
-        ctx.lineTo(x, y);
-        if(sym_H && sym_V) {
-            ctx.moveTo(canvas.width - lastX, canvas.height - lastY);
-            ctx.lineTo(canvas.width - x, canvas.height - y);
+        trace(lastX, lastY, x, y);
+        if(sym_V) {
+            trace(canvas.width - lastX, lastY, canvas.width - x, y);
         }
         if(sym_H){
-            ctx.moveTo(lastX, canvas.height - lastY);
-            ctx.lineTo(x, canvas.height - y);
+            trace(lastX, canvas.height - lastY, x, canvas.height - y);
         }
-        if(sym_V) {
-            ctx.moveTo(canvas.width - lastX, lastY);
-            ctx.lineTo(canvas.width - x, y);
+        if(sym_H && sym_V) {
+            trace(canvas.width - lastX, canvas.height - lastY, canvas.width - x, canvas.height - y);;
         }
+
     } else if(tool === 'eraser' || tool === 'pencil'){
         if(tool === 'eraser') {
             ctx.globalCompositeOperation="destination-out";
         } else {
             ctx.globalCompositeOperation="source-over";
         }
-        ctx.moveTo(x, y);
-        ctx.arc(x, y, 0.1, 0, Math.PI * 2, false);
+        trace(x, y, x, y, true);
         if(sym_H && sym_V) {
-            ctx.moveTo(canvas.width - x, canvas.height - y);
-            ctx.arc(canvas.width - x, canvas.height - y, 0.1, 0, Math.PI * 2, false);
+            // ctx.moveTo(canvas.width - x, canvas.height - y);
+            // ctx.arc(canvas.width - x, canvas.height - y, 0.1, 0, Math.PI * 2, false);
+            trace(canvas.width - x, canvas.height - y, canvas.width - x, canvas.height - y, true);
         }
         if(sym_H){
-            ctx.moveTo(x, canvas.height - y);
-            ctx.arc(x, canvas.height - y, 0.1, 0, Math.PI * 2, false);
+            // ctx.moveTo(x, canvas.height - y);
+            // ctx.arc(x, canvas.height - y, 0.1, 0, Math.PI * 2, false);
+            trace(x, canvas.height - y, x, canvas.height - y, true);
         }
         if(sym_V) {
-            ctx.moveTo(canvas.width - x, y);
-            ctx.arc(canvas.width - x, y, 0.1, 0, Math.PI * 2, false);
+            // ctx.moveTo(canvas.width - x, y);
+            // ctx.arc(canvas.width - x, y, 0.1, 0, Math.PI * 2, false);
+            trace(canvas.width - x, y, canvas.width - x, y, true);
         }
     }
-    ctx.closePath();
-    ctx.stroke();
     lastX = x; lastY = y;
 }
 
 function drawLine(x, y){
-
-    ctx.beginPath();
-    ctx.strokeStyle = $('#colorpick').val();
-    ctx.lineWidth = $('#sizepick').val();
-    ctx.lineJoin = ctx.lineCap = "round";
     if(line) {
         ctx.moveTo(x, y);
         lastX = x;
@@ -172,24 +165,18 @@ function drawLine(x, y){
         line = false;
     } else {
         ctx.globalCompositeOperation="source-over";
-        ctx.moveTo(lastX, lastY);
-        ctx.lineTo(x, y);
-        if(sym_H && sym_V) {
-            ctx.moveTo(canvas.width - lastX, canvas.height - lastY);
-            ctx.lineTo(canvas.width - x, canvas.height - y);
+        trace(lastX, lastY, x, y);
+        if(sym_V) {
+            trace(canvas.width - lastX, lastY, canvas.width - x, y);
         }
         if(sym_H){
-            ctx.moveTo(lastX, canvas.height - lastY);
-            ctx.lineTo(x, canvas.height - y);
+            trace(lastX, canvas.height - lastY, x, canvas.height - y);
         }
-        if(sym_V) {
-            ctx.moveTo(canvas.width - lastX, lastY);
-            ctx.lineTo(canvas.width - x, y);
+        if(sym_H && sym_V) {
+            trace(canvas.width - lastX, canvas.height - lastY, canvas.width - x, canvas.height - y);;
         }
         line = true
     }
-    ctx.closePath();
-    ctx.stroke();
 }
 
 function drawRec(x, y){
@@ -245,15 +232,15 @@ function drawCircle(x, y){
         var rayon = Math.sqrt(Math.pow(lastX - x, 2) + Math.pow(lastY - y, 2));
         ctx.arc(lastX,lastY,rayon,0,Math.PI*2,false);
         if(sym_H && sym_V) {
-            ctx.moveTo(canvas.width - lastX, canvas.height - lastY);
+            ctx.moveTo(canvas.width - lastX + rayon, canvas.height - lastY);
             ctx.arc(canvas.width - lastX, canvas.height - lastY,rayon,0,Math.PI*2,false);
         }
         if(sym_H){
-            ctx.moveTo(lastX, canvas.height - lastY);
+            ctx.moveTo(lastX + rayon, canvas.height - lastY);
             ctx.arc(lastX, canvas.height - lastY,rayon,0,Math.PI*2,false);
         }
         if(sym_V) {
-            ctx.moveTo(canvas.width - lastX, lastY);
+            ctx.moveTo((canvas.width + rayon) - lastX, lastY);
             ctx.arc(canvas.width - lastX,lastY,rayon,0,Math.PI*2,false);
         }
         if(fill) {
